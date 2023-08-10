89e5a7e3d24d16a50cbfd1c37ed5d5eaf9c787c7
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-08-10 15:46:04 -0700

    Formatting

--- src/lib/GraphGeneration.ts
+++ src/lib/GraphGeneration.ts
@@ -5,39 +5,39 @@ import { completion, conversation, reply, userMessage, response } from "./OpenAi
 
 const prompts = {
   buildCandidates: (data: { goal: string }) => `
-  SIMULATION: You are a computational engine that generates a concept map for the following topic: ${data.goal}
+SIMULATION: You are a computational engine that generates a concept map for the following topic: ${data.goal}
 
-  Please return a graph in the following JSON format:
+Please return a graph in the following JSON format:
 
-  \`\`\`
-  {
-    "nodes": [ "example", "node", "names" ],
-    "edges": [
-      { "source": "example", "target": "node" },
-      { "source": "node", "target": "names" }
-  }
-  \`\`\`
+\`\`\`
+{
+"nodes": [ "example", "node", "names" ],
+"edges": [
+{ "source": "example", "target": "node" },
+{ "source": "node", "target": "names" }
+}
+\`\`\`
 
-  - Nodes are the concepts, and are single strings.
-  - Edges are the relationships between concepts, and are an array of objects with a \`source\` and \`target\` property.
+- Nodes are the concepts, and are single strings.
+- Edges are the relationships between concepts, and are an array of objects with a \`source\` and \`target\` property.
 
-  Generate a comprehensive graph and then grade it on the following criteria:
+Generate a comprehensive graph and then grade it on the following criteria:
 
-  - How well organised is the graph? (0-10)
-  - How comprehensive is the graph? (0-10)
-  - Are there sources that could point to more targets (more sub concepts)? (0-10)
-  - Is the graph arranged in the order that these concepts would typically be learned? (0-10)
+- How well organised is the graph? (0-10)
+- How comprehensive is the graph? (0-10)
+- Are there sources that could point to more targets (more sub concepts)? (0-10)
+- Is the graph arranged in the order that these concepts would typically be learned? (0-10)
 
 
-  For each question, add concise reasoning as to why you picked that score. Make a concise 5 bullet list of specific changes that could be made to improve the score.
+For each question, add concise reasoning as to why you picked that score. Make a concise 5 bullet list of specific changes that could be made to improve the score.
 
-  Using the 5 bullet list, incorporate this feedback by generating another candidate, making sure to merge the result with the earlier candidate.
+Using the 5 bullet list, incorporate this feedback by generating another candidate, making sure to merge the result with the earlier candidate.
 
-  Continue this process 2 more times.
+Continue this process 2 more times.
 
-  Calculate the total rank for each candidate, show the totals.
+Calculate the total rank for each candidate, show the totals.
 
-  Pick the best one based on highest score`,
+Pick the best one based on highest score`,
 
   selectCandidate: "Return just the JSON of the highest ranked candidate.",
 };
